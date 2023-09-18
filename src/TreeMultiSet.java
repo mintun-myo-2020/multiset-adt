89c3b969e3a55a27fec09b9a2df7ac828ade97e6
Ryan Chen <ryanchen604@gmail.com>
2023-09-18 10:55:33 -0400

    uploaded treemultiset

--- src/TreeMultiSet.java
+++ src/TreeMultiSet.java
@@ -1,4 +1,26 @@
-public class TreeMultiSet extends MultiSet{
-    Tree tree;
+public class TreeMultiSet implements MultiSet {
+    private Tree tree;
 
+    public TreeMultiSet (Tree tree) {
+        this.tree = tree;
+    }
+    public boolean add (Object item) {
+        this.tree.insert(item);
+        return true;
+    }
+    public void remove (Object item) {
+        this.tree.delete_item(item);
+    }
+    public boolean contains (Object item) {
+        return this.tree.contains(item);
+    }
+    public boolean is_empty () {
+        return this.tree_is_empty;
+    }
+    public int count (Object item) {
+        return this.tree.count();
+    }
+    public int size () {
+        return this.tree.length();
+    }
 }
